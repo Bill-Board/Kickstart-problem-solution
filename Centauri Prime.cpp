6d095e8d8e8ee2aa82dc06eab935c62d62bcb0c6
Shoeb Akibul Islam <59358725+Bill-Board@users.noreply.github.com>
2023-06-26 21:51:51 +0600

    Add files via upload

---
+++ Centauri Prime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string GetRuler(const string& s) {
+  // TODO: implement this method to determine the ruler name, given the kingdom.
+  string ruler;
+  char c[]={'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+  if(s.back()=='y' or s.back()=='Y')
+    return string("nobody");
+  for(int i=0; i<10; i++)
+    if(s.back()==c[i])
+        return string("ALice");
+  return string("Bob");
+}
+
+int main() {
+  int testcases;
+  cin >> testcases;
+  string kingdom;
+
+  for (int t = 1; t <= testcases; ++t) {
+    cin >> kingdom;
+    cout << "Case #" << t << ": " << kingdom << " is ruled by "
+         << GetRuler(kingdom) << ".\n";
+  }
+  return 0;
+}
---
+++ Sample.cpp
@@ -0,0 +1,47 @@
+//BISMILLAHIR RAHMANIR RAHEEM
+//ALLAH IS WATCHING ME
+// Shoeb Akibul Islam
+// Dept of ICE, NSTU
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long v99;
+#define vll    vector<v99>
+#define dua ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define i_love_u_huu dua long long tokp;cin >> tokp;while(tokp--)
+#define ses   "\n"
+#define whp   " "
+#define sob(z)  (z).begin(), (z).end()
+#define ff first
+#define ss second
+#define pii pair<v99, v99>
+const v99 N=200005;
+v99 tc = 1;
+
+/// code is here->----------------------------
+
+void solve()
+{
+    v99 n , m;
+    cin >> n >> m;
+    vll a(n);
+    for(auto &i : a)cin  >> i;
+    cout <<"Case #"<<tc++<<": "<<accumulate(sob(a),0LL) % m <<ses;
+}
+
+
+
+signed main()
+{
+    //dua
+    //freopen("data.out","w",stdout);
+    //while(1)
+    i_love_u_huu
+    solve();
+    return 0;
+}
+
+
+
+///  Alhamdulillah...
